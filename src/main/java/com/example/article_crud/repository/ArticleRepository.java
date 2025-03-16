a7545647dffbffcbc10db4177e12c079a50e16e1
Victor Ferre <victor522m@gmail.com>
2025-03-16 19:54:14 +0100

    modified:   src/main/java/com/example/article_crud/repository/ArticleRepository.java

--- src/main/java/com/example/article_crud/repository/ArticleRepository.java
+++ src/main/java/com/example/article_crud/repository/ArticleRepository.java
@@ -3,5 +3,5 @@ package com.example.article_crud.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.article_crud.entity.Article;
 
-public interface ArticleRepository extends JpaRepository<Article, Integer> { // ID ahora es Integer
+public interface ArticleRepository extends JpaRepository<Article, Integer> {  
 }
